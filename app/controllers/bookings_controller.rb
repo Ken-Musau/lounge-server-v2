141faf5b2144aa33758759d65b6128bf1d883bb6
Kennedy Musau <kennedy.musauu@gmail.com>
2023-12-04 02:38:18 +0300

    update booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,51 +1,50 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: %i[ show update destroy ]
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-  # GET /bookings
   def index
-    @bookings = Booking.all
-
-    render json: @bookings
+   render json: Booking.all
   end
 
-  # GET /bookings/1
   def show
-    render json: @booking
+    booking = find_booking
+    render json: booking
   end
 
-  # POST /bookings
   def create
-    @booking = Booking.new(booking_params)
-
-    if @booking.save
-      render json: @booking, status: :created, location: @booking
-    else
-      render json: @booking.errors, status: :unprocessable_entity
-    end
+    booking = booking.create!(booking_params)
+    render json: booking, status: :created
   end
 
-  # PATCH/PUT /bookings/1
   def update
-    if @booking.update(booking_params)
-      render json: @booking
-    else
-      render json: @booking.errors, status: :unprocessable_entity
-    end
+    booking = find_booking
+    booking.update!(booking_params)
+    render json: booking
   end
 
-  # DELETE /bookings/1
   def destroy
-    @booking.destroy!
+    booking = find_booking
+    booking.destroy
+    head :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_booking
-      @booking = Booking.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def booking_params
-      params.require(:booking).permit(:status, :check_in, :check_out, :space_id, :user_id, :amount)
-    end
+  def find_booking
+    Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permint(:status, :check_in, :check_out, :amount, :user_id, :space_id)
+  end
+
+
+  def render_not_found
+    render json: { error: "Booking not found"}, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages  }, status: :unprocessable_entity
+  end
+
 end
