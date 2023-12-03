7b5d46ca016485c9517dfa106f0bc62341814a6b
Kennedy Musau <kennedy.musauu@gmail.com>
2023-12-04 01:27:43 +0300

    setup middleware for cookies

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,14 @@ module LoungeServerV2
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
+
+       # ▾ Must add these lines! ▾
+    # Adding back cookies and session middleware
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    # Use SameSite=Strict for all cookies to help protect against CSRF
+    config.action_dispatch.cookies_same_site_protection = :strict
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
