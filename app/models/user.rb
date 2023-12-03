1e59c3f03dfbbc620902b8aede9c16e3da2f6db9
Kennedy Musau <kennedy.musauu@gmail.com>
2023-12-04 02:34:06 +0300

    updae user modle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+
+   enum role: [:client, :admin], default: :client
+   has_many :spaces, dependent: :destroy
+   has_many :bookings, dependent: :destroy
+   has_many :reviews, dependent: :destroy
+   validates :username, uniqueness: { case_sensitive: false }
+
 end
