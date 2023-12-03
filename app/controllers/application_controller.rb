86cb09c62246b5507276a5ddfb1d253644a0207c
Kennedy Musau <kennedy.musauu@gmail.com>
2023-12-04 02:37:05 +0300

    update auth in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,46 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  before_action :authorized
 
-  def hello_world
-    session[:count] = (session[:count] || 0) + 1
-    render json: { count: session[:count] }
+  def encode_token(payload)
+    JWT.encode(payload, 'my_s3cr3t')
   end
+
+
+  def decoded_token
+    header = request.headers['Authorization']
+    if header
+        token = header.split(" ")[1]
+        begin
+            JWT.decode(token, 'my_s3cr3t')
+        rescue JWT::DecodeError
+            nil
+        end
+    end
+  end
+
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
+
+
+
 end
+
+
+# def hello_world
+#   session[:count] = (session[:count] || 0) + 1
+#   render json: { count: session[:count] }
+# end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :first_name, :last_name, :email, :avatar, :password, :role, :contact
+  has_many :bookings
+  has_many :reviews
 end
