90ae562b49fe0bcbdb3cd5e7112eb71905d2ec79
Kennedy Musau <kennedy.musauu@gmail.com>
2023-12-04 02:41:11 +0300

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,63 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show update destroy ]
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  skip_before_action :authorized, only: [:create, :index]
 
-  # GET /users
   def index
-    @users = User.all
-
-    render json: @users
+    users = User.all
+    render json: users
   end
 
-  # GET /users/1
   def show
-    render json: @user
+    user = find_user
+    render json: user
   end
 
-  # POST /users
+
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    user = User.create!(user_params)
+    @token = encode_token( user_id: user.id )
+    render json: {user: UserSerializer.new(user), jwt: @token}, status: :created
   end
 
-  # PATCH/PUT /users/1
+  def me
+    # render json: current_user, status: :accepted
+    render json: current_user , status: :accepted
+  end
+
   def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    user = find_user
+    user.update!(user_params)
+    render json: user
   end
 
-  # DELETE /users/1
   def destroy
-    @user.destroy!
+    user = find_user
+    user.destroy
+    head :no_content
   end
+
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+
+  def find_user
+    User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(:username, :first_name, :last_name, :email, :avatar, :password, :role, :contact)
+  end
+
+  def render_not_found
+    render json: { error: "User not found"}, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages  }, status: :unprocessable_entity
+  end
 
     # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:username, :first_name, :last_name, :email, :avatar, :password, :password_confirmation, :role, :contact)
-    end
+    # def user_params
+    #   params.require(:user).permit(:username, :first_name, :last_name, :email, :avatar, :password, :password_confirmation, :role, :contact)
+    # end
 end
